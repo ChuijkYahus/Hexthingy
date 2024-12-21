551c569150db1fe877526c831ebf76bfc0ac9445
Mylickr <mylickyt@gmail.com>
2024-12-21 13:49:36 +0500

    smite rework

--- src/main/java/me/uwutaku/hexthingy/Hexthingy.java
+++ src/main/java/me/uwutaku/hexthingy/Hexthingy.java
@@ -1,21 +1,29 @@
 package me.uwutaku.hexthingy;
 
+import at.petrak.hexcasting.api.addldata.ADMediaHolder;
 import at.petrak.hexcasting.api.casting.ActionRegistryEntry;
 import at.petrak.hexcasting.api.casting.castables.Action;
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
 import at.petrak.hexcasting.api.casting.math.HexDir;
 import at.petrak.hexcasting.api.casting.math.HexPattern;
+import at.petrak.hexcasting.api.utils.MediaHelper;
 import at.petrak.hexcasting.common.lib.hex.HexActions;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 
+import java.util.List;
 import java.util.Optional;
 
 public class Hexthingy implements ModInitializer {
 
 	public static final String MODID = "hexthingy";
-//test
+
 	@Override
 	public void onInitialize() {
 		registerAction("smite", "wweeewwweeeqeeeadweeead", HexDir.EAST, new Smite());
@@ -33,6 +41,15 @@ public class Hexthingy implements ModInitializer {
 						new ActionPredicateWrapper(action, (CastingEnvironment env) -> Optional.empty())
 				)
 		);
+	}
+	public static long getTotalMediaFromPlayer(ServerPlayerEntity player){
+
+		long media = 0;
+		List<ADMediaHolder> media_holders = MediaHelper.scanPlayerForMediaStuff(player);
+		for (ADMediaHolder holder : media_holders){
+			media += holder.getMedia();
+		}
+		return media;
 	}
 
 }
--- src/main/java/me/uwutaku/hexthingy/Smite.kt
+++ src/main/java/me/uwutaku/hexthingy/Smite.kt
@@ -1,12 +1,19 @@
 package me.uwutaku.hexthingy
 
+import at.petrak.hexcasting.api.addldata.ADMediaHolder
 import at.petrak.hexcasting.api.casting.*
 import at.petrak.hexcasting.api.casting.castables.SpellAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.misc.MediaConstants
+import at.petrak.hexcasting.api.utils.scanPlayerForMediaStuff
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.damage.DamageSource
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.registry.RegistryKeys
+import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
+
 
 class Smite : SpellAction {
     override val argc = 2
@@ -18,23 +25,26 @@ class Smite : SpellAction {
         val target = args.getLivingEntityButNotArmorStand(0)
         val dmg = args.getDouble(1)
         env.assertEntityInRange(target)
-
         val cost = dmg * MediaConstants.SHARD_UNIT * 1.5
         return SpellAction.Result(
-            Spell(target, dmg),
+            Spell(target, dmg, env.castingEntity!!),
             cost.toLong(),
             listOf(ParticleSpray.cloud(target.pos.add(0.0, target.eyeY / 2.0, 0.0), 1.0))
         )
     }
 
-    private class Spell(val target: LivingEntity, val dmg: Double) :
+    private class Spell(val target: LivingEntity, val dmg: Double, val caster: LivingEntity) :
         RenderedSpell {
         override fun cast(env: CastingEnvironment) {
-
             if (target.world is ServerWorld) {
-                val serverWorld = target.world as ServerWorld
-                val magicDamage = serverWorld.damageSources.magic()
-                target.damage(magicDamage, dmg.toFloat())
+                if(target is PlayerEntity){
+                    //make here reverse damage
+                    val caster_media_amount = Hexthingy.getTotalMediaFromPlayer(caster as ServerPlayerEntity)
+                    val target_media_amount = Hexthingy.getTotalMediaFromPlayer(target as ServerPlayerEntity)
+
+                    caster.damage(DamageTypes.of(caster.world, DamageTypes.smitecaster_damage), 1.0f)
+                }
+                target.damage(DamageTypes.of(target.world, DamageTypes.smiterecipient_damage), dmg.toFloat())
             }
         }
     }
